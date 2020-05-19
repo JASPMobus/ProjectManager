50db1c0a6db7620ed355c69ab3f96c9e83404115
JASPMobus <mobusj@live.com>
2020-05-19 14:40:08 -0400

    Add validation for projects and changed the direction they recognize owners.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,15 @@
 class Project < ApplicationRecord
     #Associations
-    belongs_to :user
     has_many :tasks
     has_many :users, through: :tasks
+
+    #Validations
+    #The project must have a name that the user hasn't already used for a different project.
+    validates :name, presence: true
+    validates :name, uniqueness: { scope: [:owner] }
+
+    #Methods
+    def owner
+        User.find(owner_id)
+    end
 end

c4523b1aafe076ff8a3040ada45a22010605fc67
JASPMobus <mobusj@live.com>
2020-05-19 19:08:05 -0400

    Added edit functionality to projects, added a link to project show page to create a new linked task.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,6 +20,23 @@ class ProjectsController < ApplicationController
         end
     end
 
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        @project.update(project_params)
+
+        if @project.valid?
+            @project.save
+
+            redirect_to project_path(@project)
+        else
+            render :edit
+        end
+    end
+
     def show
         @project = Project.find(params[:id])
     end

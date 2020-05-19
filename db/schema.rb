e4f2d59d99f85801e5b5fbbe9f42f24efb342d91
JASPMobus <mobusj@live.com>
2020-05-19 14:19:45 -0400

    Add tasks and relationships between models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_180711) do
+ActiveRecord::Schema.define(version: 2020_05_19_181223) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_05_19_180711) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "owner_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "summary"
+    t.text "description"
+    t.integer "project_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
